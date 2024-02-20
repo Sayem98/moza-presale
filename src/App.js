31d3218ba681ce9bd6e697a63e233f3a62dc7750
Sayem98 <sayemabedin.bd@gmail.com>
2024-02-20 22:01:01 +0600

    Presale: wallet connect system with Web3Modal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./Pages/Main";
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react";
 import "./css/App.scss";
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Sidebar from "./components/Sidebar";
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = "70041514f5ce6f21b906515e17a076e3";
 
-import Main from "./Pages/Main";
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: "Ethereum",
+  currency: "ETH",
+  explorerUrl: "https://etherscan.io",
+  rpcUrl: "https://cloudflare-eth.com",
+};
+
+// 3. Create modal
+const metadata = {
+  name: "My Website",
+  description: "My Website description",
+  url: "https://mywebsite.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.mywebsite.com/"],
+};
+
+createWeb3Modal({
+  ethersConfig: defaultConfig({ metadata }),
+  chains: [mainnet],
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+});
 
 function App() {
-  const [openSidebar, setOpenSidebar] = useState(false);
-
   return (
     <div className="App">
       <BrowserRouter>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 import Modal from "../components/Modal";
 import Countdown from "react-countdown";
 import ConnectWalletPopup from "./ConnectWalletPopup";
+import { useDisconnect, useWeb3Modal } from "@web3modal/ethers/react";
+import { useWeb3ModalAccount } from "@web3modal/ethers/react";
 
 const Header = ({ openSidebar, setOpenSidebar }) => {
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
+
+  const { open } = useWeb3Modal();
+  const { address, chainId, isConnected } = useWeb3ModalAccount();
 
   return (
     <div className="header-camp flex">
@@ -13,7 +18,7 @@ const Header = ({ openSidebar, setOpenSidebar }) => {
         <div className="left flex items-center">
           <Link to={"/"}>
             <div className="app-logo flex aic cursor-pointer">
-              <img src="./images/logo.png" className="logo-img " />
+              <img src="./images/logo.png" alt="LOGO" className="logo-img " />
               <div className="logo-txt logo-font cfff">Barkley</div>
             </div>
           </Link>
@@ -24,15 +29,25 @@ const Header = ({ openSidebar, setOpenSidebar }) => {
             <a href="#howBuy" className="btn button">
               HOW TO BUY Barkley
             </a>
-            <div className="btn button" onClick={(e) => setOpen(true)}>
-              CONNECT WALLET
+
+            <div
+              className="btn button"
+              onClick={
+                isConnected
+                  ? () => open({ view: "Account" })
+                  : () => open({ view: "Connect" })
+              }
+            >
+              {isConnected
+                ? address.slice(0, 6) + "..." + address.slice(-4)
+                : "Connect Wallet"}
             </div>
           </div>
         </div>
       </div>
-      <Modal open={open} onClose={() => setOpen(false)}>
+      {/* <Modal open={open} onClose={() => setOpen(false)}>
         <ConnectWalletPopup setOpen={setOpen} />
-      </Modal>
+      </Modal> */}
     </div>
   );
 };
